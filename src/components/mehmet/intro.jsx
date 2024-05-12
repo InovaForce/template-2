b5a1dad518c7075ba8f0b33db6f5633d08c5811d
Mehmet Genç <gencaslan555@gmail.com>
2024-05-13 00:43:42 +0300

    mehmet

---
+++ src/components/mehmet/intro.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './intro.scss'
+
+const Intro = () => {
+  return (
+    <section className="intro">
+      <div className="container">
+        <h1>Miraç Kandili &darr;</h1>
+      </div>
+    </section>
+  );
+}
+
+export default Intro
