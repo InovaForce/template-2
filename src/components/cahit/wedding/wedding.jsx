592f6df1f35657f03321340e1d7113310e683818
ata324 <mavican324@gmail.com>
2024-05-20 16:41:02 +0200

    cahit

--- src/components/cahit/wedding/wedding.jsx
+++ src/components/cahit/wedding/wedding.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
 import WeddingCard from "./weddingCard";
-import Spacer from "../galerry/spacer";
+import "./wedding.scss";
 
 const Wedding = () => {
   return (
     <div className="wedding">
-      <Container className="bg-white">
-      <h2 className="text-warning text-center">WEDDING</h2>
-      <h6 className="text-dark text-center wedding-our">Our upcoming Weddings</h6>
-      <Spacer height="40px"/>
+      <Container >
+      <h2 className=" text-center">WEDDING</h2>
+      <h6 className="text-dark text-center wedding-our mb-5">Our upcoming Weddings</h6>
+     
         <Row  sm={1} md={2} lg={3} className="g-4">
           <WeddingCard />
         </Row>
