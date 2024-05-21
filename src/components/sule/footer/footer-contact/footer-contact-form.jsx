a431dca56422f3f7594d994b10843ae672a93f3a
Sule Demir <sule.sozturk@hotmail.com>
2024-05-21 18:45:09 +0300

    footer improved

--- src/components/sule/footer/footer-contact/footer-contact-form.jsx
+++ src/components/sule/footer/footer-contact/footer-contact-form.jsx
@@ -1,53 +1,44 @@
 "use client";
-import React from "react";
-import "bootstrap/dist/css/bootstrap.css";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import { Container } from "react-bootstrap";
-import "./footer-contact-form.scss";
-const FooterContactForm = () => {
+import { useState } from "react";
+import "./modal.scss"; 
+import { Button } from "react-bootstrap";
+import ModalContact from "./modal-contact";
+import FooterContactForm from "./footer-contact-form";
+
+const Modal = () => {
+  const [modalActive, setModalActive] = useState(false);
+
+  const handleModalClick = () => {
+    setModalActive(!modalActive);
+  };
+
+  const handleModalContentClick = (e) => {
+    e.stopPropagation();
+  };
+
   return (
-    
-    <Container
-      className="footer-contact-box"
-      
-    >
-      <h4>Contact Us</h4>
-      <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h5>
-      <Form className="form ">
-        <Form.Group>
-          <Form.Control
-            className="my-2 bg bg-info-subtle"
-            type="text"
-            placeholder="Enter your full name"
-          />
-          <Form.Control.Feedback type="invalid">
-            Invalid name
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group>
-          <Form.Control
-            className="my-2 bg bg-info-subtle"
-            type="email"
-            placeholder="Enter your email address"
-          />
-          <Form.Control.Feedback type="invalid">
-            Invalid email
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group className="mb-3" size="lg">
-          <Form.Control
-            className="my-2 bg"
-            as="textarea"
-            placeholder="Your message"
-          />
-        </Form.Group>
-        <Button variant="btn-primary" type="submit">
-          Send Message
-        </Button>
-      </Form>
-    </Container>
+    <div className={`modal-container ${modalActive ? "active" : ""}`}>
+      <Button
+        className="copyright-link"
+        variant="primary"
+        onClick={handleModalClick}
+      >
+        Open Modal
+      </Button>
+      {modalActive && (
+        <div
+          className="modal-background"
+          onClick={handleModalClick}
+        >
+          <div className="modal" onClick={handleModalContentClick}>
+            <h2>Contact Us</h2>
+            <FooterContactForm />
+            <ModalContact />
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
-export default FooterContactForm;
+export default Modal;
